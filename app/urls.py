f8e613b5ab0b7c1bf738f50c73a2f3dc7a724565
kunal singhal <45396838+kunal-9@users.noreply.github.com>
2020-07-16 21:59:06 +0530

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
 
     path('contacts',views.contacts,name = 'contacts'),
     path('donate',views.donate,name = 'donate'),
+    path('gallery',views.gallery,name = 'gallery'),
+    path('team',views.team,name = 'team'),
 
     path('',views.home,name = 'home'),
     
